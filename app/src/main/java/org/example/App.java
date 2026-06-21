5c5bd375324cd52d5a46eb6011213a12aa8031c5
Bex Kuriakose <b_kuriakose@mail.harpercollege.edu>
2026-06-21 03:01:01 -0500

    Completed prime factorization lab

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,5 +1,5 @@
-package org.example;
 
+package org.example;
 
 public class App {
   public static void main(String[] args) {
--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -1,9 +1,9 @@
+
 package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
-
+        import java.util.ArrayList;
 
 class FactorizerTest {
   @Test
