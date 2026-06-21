03b8f64459769d3e926786a07be5e336e61bc768
Bex Kuriakose <b_kuriakose@mail.harpercollege.edu>
2026-06-21 02:45:51 -0500

    Complete prime factorization lab

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,16 @@
 package org.example;
 
+
 public class App {
   public static void main(String[] args) {
-    // Test your Factorizer class using a driver program here!
+    Factorizer factorizer = new Factorizer();
+    System.out.println("Prime factors of 0: " + factorizer.primeFactors(0));
+    System.out.println("Prime factors of 1: " + factorizer.primeFactors(1));
+    System.out.println("Prime factors of 9: " + factorizer.primeFactors(9));
+    System.out.println("Prime factors of 10: " + factorizer.primeFactors(10));
+    System.out.println("Prime factors of 24: " + factorizer.primeFactors(24));
+    System.out.println("Prime factors of 100: " + factorizer.primeFactors(100));
   }
 }
+
+
--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -1,13 +1,56 @@
 package org.example;
 
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+
 
 class FactorizerTest {
   @Test
   void itReturnsAnEmptyListForZero() {
     Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+    ArrayList<Integer> expected = new ArrayList<>();
+    assertEquals(expected, factorizer.primeFactors(0));
+  }
+
+
+  @Test
+  void itReturnsAnEmptyListForOne() {
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> expected = new ArrayList<>();
+    assertEquals(expected, factorizer.primeFactors(1));
+  }
+
+
+  @Test
+  void itReturnsFactorsForNine() {
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> expected = new ArrayList<>();
+    expected.add(3);
+    expected.add(3);
+    assertEquals(expected, factorizer.primeFactors(9));
+  }
+
+
+  @Test
+  void itReturnsFactorsForTen() {
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> expected = new ArrayList<>();
+    expected.add(2);
+    expected.add(5);
+    assertEquals(expected, factorizer.primeFactors(10));
+  }
+
+
+  @Test
+  void itReturnsFactorsForOneHundred() {
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> expected = new ArrayList<>();
+    expected.add(2);
+    expected.add(2);
+    expected.add(5);
+    expected.add(5);
+    assertEquals(expected, factorizer.primeFactors(100));
   }
 }
